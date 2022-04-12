40cc6b6030035c45261a503b47a96dcb5b087562
Natasha Mboya <natashamboya@Natashas-MBP.lan>
2022-04-11 21:34:17 -0400

    full redo of carols website

--- app.js
+++ app.js
@@ -3,18 +3,21 @@
  */
 
 var static = require('node-static');
-
+console.log("Starting...")
 //
 // Create a node-static server instance to serve the './public' folder
 //
 var file = new static.Server('./public');
 
+console.log("Creating server")
 require('http').createServer(function (request, response) {
     request.addListener('end', function () {
         //
         // Serve files!
         //
+        console.log('Serving files...')
         file.serve(request, response);
     }).resume();
+    console.log('Listening on port 8080...')
 }).listen(8080);
 
